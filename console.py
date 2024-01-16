c3effee6221d53439a82b7081b08ef183a35eadc
Ebi-Fredrick Emmanuel <ebifredrick07@gmail.com>
2024-01-16 23:54:55 +0000

    General code update

--- console.py
+++ console.py
@@ -5,19 +5,34 @@
 import cmd
 import sys
 
-
 class HBNBCommand(cmd.Cmd):
     """HBNBCommand class."""
-
+    
     prompt = '(hbnb) '
 
     def do_help(self, line):
         """Display help."""
-
+        # Your help logic here
+    
     def do_quit(self, line):
         """Quit the shell."""
         return True
 
+    def default(self, line):
+        """Handle unknown syntax."""
+        if line == 'EOF':
+            sys.exit(0)
+        else:
+            self.stdout.write('*** Unknown syntax: %s\n' % line)
+
+    def emptyline(self):
+        """Handle empty line"""
+        pass
+
+
 if __name__ == '__main__':
-    HBNBCommand().cmdloop()
+    try:
+        HBNBCommand().cmdloop()
+    except KeyboardInterrupt:
+        print("\nExiting...")
 
--- models/base_model.py
+++ models/base_model.py
@@ -5,32 +5,39 @@
 import uuid
 from datetime import datetime
 
-
-class BaseMode1:
-    """BaseMode1 class"""
-
+class BaseModel:
+    """BaseModel class."""
+    
     def __init__(self, *args, **kwargs):
-        """Initialize BaseMode1"""
-        self.id = str(uuid.uuid4())
-        self.created_at = self.updated_at = datetime.now()
+        """Initialize BaseModel."""
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue  # Skip __class__ attribute
+                if key in ['created_at', 'updated_at']:
+                    setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.now()
 
     def __str__(self):
-        """String representation of BaseMode1"""
-
+        """String representation of BaseModel."""
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        """Update Updated_at with the current datetime"""
+        """Update updated_at with the current datetime."""
         self.updated_at = datetime.now()
-        
 
     def to_dict(self):
-        """Convert instance to dictionary"""
+        """Return a dictionary containing all keys/values of __dict__."""
         obj_dict = self.__dict__.copy()
         obj_dict['__class__'] = self.__class__.__name__
         obj_dict['created_at'] = self.created_at.isoformat()
         obj_dict['updated_at'] = self.updated_at.isoformat()
-        return obj_dictecho "help" | ./console.py
+        return obj_dict
+
 
     def from_dict(self, dict_data):
         """Create instance from dictionary data"""
