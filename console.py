af9629a5a26440484134fae2dc7922d94471af04
Ebi-Fredrick Emmanuel <ebifredrick07@gmail.com>
2024-01-16 19:22:18 -0800

    Creation of create command

--- console.py
+++ console.py
@@ -2,14 +2,17 @@
 
 """Interactive shell for Airbnb project."""
 
+import json
 import cmd
 import sys
+from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
     """HBNBCommand class."""
 
     prompt = '(hbnb) '
+    supported_classes = ["BaseModel"]
 
     def do_help(self, line):
         """Display help."""
@@ -29,6 +32,20 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """Handle empty line"""
         pass
+
+    def do_create(self, class_):
+        """Creates a new instance of a given class and saves it to a given class"""
+        supported_classes = ["BaseModel"]
+        if not class_:
+            print("** class name missing **")
+        elif class_ not in supported_classes:
+            print("** class doesn't exist **")
+        else:
+            instance = BaseModel()
+            print(instance.id)
+            inst_dict = instance.to_dict()
+            with open(file="data.json", mode="w") as data_file:
+                json.dump(inst_dict, data_file, indent=4)
 
 
 if __name__ == '__main__':
